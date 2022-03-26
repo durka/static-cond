66b294dde9fa0dba0f3b3eeb1c0d019e8a97ca67
Alex Burka <aburka@exyntechnologies.com>
2022-03-26 16:34:32 -0400

    consolidate private rules to first macro

--- src/lib.rs
+++ src/lib.rs
@@ -31,31 +31,41 @@
 #[macro_export]
 macro_rules! static_cond {
     // private rule to define and call the local macro
-    (@go $lhs:tt $rhs:tt $arm1:tt $arm2:tt) => {{
+    // we evaluate a conditional by generating a new macro (in an inner scope, so name shadowing is
+    // not a big concern) and calling it
+    (@expr $lhs:tt $rhs:tt $arm1:tt $arm2:tt) => {{
         // note that the inner macro has no captures (it can't, because there's no way to escape `$`)
         macro_rules! __static_cond {
             ($lhs $lhs) => $arm1;
             ($lhs $rhs) => $arm2;
         }
 
-        __static_cond!{$lhs $rhs}
+        __static_cond!($lhs $rhs)
     }};
+    (@item $lhs:tt $rhs:tt $arm1:tt $arm2:tt) => {
+        // note that the inner macro has no captures (it can't, because there's no way to escape `$`)
+        macro_rules! __static_cond {
+            ($lhs $lhs) => $arm1;
+            ($lhs $rhs) => $arm2;
+        }
+
+        __static_cond! { $lhs $rhs }
+    };
 
     // no else condition provided: fall through with empty else
     (if $lhs:tt == $rhs:tt $then:tt) => {
-        $crate::static_cond!{if $lhs == $rhs $then else { }}
+        $crate::static_cond!(if $lhs == $rhs $then else { })
     };
     (if $lhs:tt != $rhs:tt $then:tt) => {
-        $crate::static_cond!{if $lhs != $rhs $then else { }}
+        $crate::static_cond!(if $lhs != $rhs $then else { })
     };
 
-    // we evaluate a conditional by generating a new macro (in an inner scope, so name shadowing is
-    // not a big concern) and calling it
+    // main entry point with then and else arms
     (if $lhs:tt == $rhs:tt $then:tt else $els:tt) => {
-        $crate::static_cond!{@go $lhs $rhs $then $els}
+        $crate::static_cond!(@expr $lhs $rhs $then $els)
 	};
     (if $lhs:tt != $rhs:tt $then:tt else $els:tt) => {
-        $crate::static_cond!{@go $lhs $rhs $els $then}
+        $crate::static_cond!(@expr $lhs $rhs $els $then)
     };
 }
 
@@ -91,31 +101,19 @@ macro_rules! static_cond {
 /// macro expansion (check `rustc --pretty=expanded`).
 #[macro_export]
 macro_rules! static_cond_item {
-    // private rule to define and call the local macro
-    (@go $lhs:tt $rhs:tt $arm1:tt $arm2:tt) => {
-        // note that the inner macro has no captures (it can't, because there's no way to escape `$`)
-        macro_rules! __static_cond {
-            ($lhs $lhs) => $arm1;
-            ($lhs $rhs) => $arm2;
-        }
-
-        __static_cond! {$lhs $rhs}
-    };
-
     // no else condition provided: fall through with empty else
     (if $lhs:tt == $rhs:tt $then:tt) => {
-        $crate::static_cond_item! {if $lhs == $rhs $then else { }}
+        $crate::static_cond_item! { if $lhs == $rhs $then else { } }
     };
     (if $lhs:tt != $rhs:tt $then:tt) => {
-        $crate::static_cond_item! {if $lhs != $rhs $then else { }}
+        $crate::static_cond_item! { if $lhs != $rhs $then else { } }
     };
 
-    // we evaluate a conditional by generating a new macro (in an inner scope, so name shadowing is
-    // not a big concern) and calling it
+    // main entry point with then and else arms
     (if $lhs:tt == $rhs:tt $then:tt else $els:tt) => {
-        $crate::static_cond_item! {@go $lhs $rhs $then $els}
+        $crate::static_cond! { @item $lhs $rhs $then $els }
     };
     (if $lhs:tt != $rhs:tt $then:tt else $els:tt) => {
-        $crate::static_cond_item! {@go $lhs $rhs $els $then}
+        $crate::static_cond! { @item $lhs $rhs $els $then }
     };
 }
